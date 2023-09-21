a70c047365d1fc2de21dc5d6a9b97ac01d7fc22d
kanghanstar <kanghanstar@outlook.com>
2023-09-21 19:06:07 +0900

    mssql 연결을 위한 내용 추가 시작

--- src/database/db.connection.ts
+++ src/database/db.connection.ts
@@ -1,1 +1,21 @@
-// DB 연결 
+import sql, { type config } from 'mssql';
+
+export default class DBService {
+    async connect() {
+        const config: config = {
+            user: 'username', // better stored in an app setting such as process.env.DB_USER
+            password: 'password', // better stored in an app setting such as process.env.DB_PASSWORD
+            server: 'your_server.database.windows.net', // better stored in an app setting such as process.env.DB_SERVER
+            port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+            database: 'AdventureWorksLT', // better stored in an app setting such as process.env.DB_NAME
+            authentication: {
+                type: 'default',
+            },
+            options: {
+                encrypt: true,
+            },
+        };
+
+        await sql.connect(config);
+    }
+}
---
+++ src/model/dbconfig.model.ts
@@ -0,0 +1,9 @@
+class DBConfig {
+    public port: number;
+
+    constructor() {
+        this.port = -1;
+    }
+}
+
+export { DBConfig };

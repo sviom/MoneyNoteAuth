80201c6b233fd52d7b83703b95185462e5e3bf6f
kanghanstar <kanghanstar@outlook.com>
2023-10-03 17:33:41 +0900

    토큰확인 로직 추가 시작

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -132,6 +132,41 @@ export default class AuthService {
 
         return { accessToken, refreshToken };
     }
+
+    async signIn2(accessToken: string, refreshToken: string) {
+        // Access Token의 유효 기간을 짧게 설정한다.
+        // Refresh Token의 유효 기간은 길게 설정한다.
+        // 사용자는 Access Token과 Refresh Token을 둘 다 서버에 전송하여 전자로 인증하고 만료됐을 시 후자로 새로운 Access Token을 발급받는다.
+        // 공격자는 Access Token을 탈취하더라도 짧은 유효 기간이 지나면 사용할 수 없다.
+        // 정상적인 클라이언트는 유효 기간이 지나더라도 Refresh Token을 사용하여 새로운 Access Token을 생성, 사용할 수 있음.
+
+        // 데이터베이스에 각 사용자에 1대1로 맵핑되는 Access Token, Refresh Token 쌍을 저장한다.
+        // 정상적인 사용자는 기존의 Access Token으로 접근하며 서버측에서는 데이터베이스에 저장된 Access Token과 비교하여 검증한다.
+        // 공격자는 탈취한 Refresh Token으로 새로 Access Token을 생성한다. 그리고 서버측에 전송하면 서버는 데이터베이스에 저장된 Access Token과 공격자에게 받은 Access Token이 다른 것을 확인한다.
+        // 만약 데이터베이스에 저장된 토큰이 아직 만료되지 않은 경우, 즉 굳이 Access Token을 새로 생성할 이유가 없는 경우 서버는 Refresh Token이 탈취당했다고 가정하고 두 토큰을 모두 만료시킨다.
+        // 이 경우 정상적인 사용자는 자신의 토큰도 만료됐으니 다시 로그인해야 한다. 하지만 공격자의 토큰 역시 만료됐기 때문에 공격자는 정상적인 사용자의 리소스에 접근할 수 없다.
+
+        jwt.verify(accessToken, CryptoService.key, (error, decode) => {
+            console.error(error);
+            console.log('decode : ', decode);
+        });
+
+        // DB의 Token 확인
+        // 기록에 없으면? => 최초 로그인 => 토큰 생성
+        // AccessToken이 다르면 => RefreshToekn이 일치하는지 확인
+        // 같으면 => 발급
+        // Access token이 만료기간 전이면? => 에러
+
+        // 인증키 만료될 경우
+        jwt.sign('', CryptoService.key, { expiresIn: '7d' });
+
+        // 인증토큰 발급
+        // Access Token, Refresh token 발급
+        // const accessToken = jwt.sign({ name: user.name, email: user.email }, CryptoService.key, { expiresIn: '7 days' });
+        // const refreshToken = jwt.sign({ name: user.name, email: user.email }, CryptoService.key, { expiresIn: '30d' });
+
+        // return { accessToken, refreshToken };
+    }
 }
 
 export { validate };
